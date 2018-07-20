bec3f7fa3313b5c3abdbcb277e4ac169d20456a9
Hani Elmalky <hani.elmalky@gmail.com>
2018-07-20 16:29:37 -0700

    update first file

--- popular_times.py
+++ popular_times.py
@@ -1,3 +1,17 @@
 import pandas as pd
 
+filename = 'chicago.csv'
 
+# load data file into a dataframe
+df = 
+
+# convert the Start Time column to datetime
+df['Start Time'] =
+
+# extract hour from the Start Time column to create an hour column
+df['hour'] =
+
+# find the most common hour (from 0 to 23)
+popular_hour = 
+    
+print('Most Frequent Start Hour:', popular_hour)
